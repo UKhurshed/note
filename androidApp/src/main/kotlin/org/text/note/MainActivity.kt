324691ed4b4ffca2f2124a8f1e5cf2812d39d5da
UKhurshed <khurshedumarov777@gmail.com>
2026-05-23 17:12:14 +0500

    добавлены .gitignore и README создана структура папок и базовый MainActivity

--- androidApp/src/main/kotlin/org/text/note/MainActivity.kt
+++ androidApp/src/main/kotlin/org/text/note/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -14,16 +21,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-//            Scaffold {
-                App()
-//            }
-
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Text("Notes App — каркас готов")
+                }
+            }
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
