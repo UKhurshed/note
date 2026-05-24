bfd822220856aaf0364a7337d176339074f77f74
UKhurshed <khurshedumarov777@gmail.com>
2026-05-24 17:54:54 +0500

    feat: переписан MainComponent на BaseStoreComponent

--- androidApp/src/main/kotlin/org/text/note/feature/main/MainComponent.kt
+++ androidApp/src/main/kotlin/org/text/note/feature/main/MainComponent.kt
@@ -1,13 +1,23 @@
 package org.text.note.feature.main
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.mvikotlin.core.instancekeeper.getStore
+import org.text.note.core.BaseStoreComponent
 
 class MainComponent(
     componentContext: ComponentContext,
+    storeFactory: MainStoreFactory,
     private val onAddNote: () -> Unit,
     private val onOpenNote: (String) -> Unit
-) : ComponentContext by componentContext {
-
+) : BaseStoreComponent<MainStore.State, MainStore.Intent, MainStore.Label>(
+    componentContext = componentContext,
+    store = componentContext.instanceKeeper.getStore { storeFactory.create() }
+) {
+    init {
+        accept(MainStore.Intent.Load)
+    }
     fun onAddClicked() = onAddNote()
     fun onNoteClicked(id: String) = onOpenNote(id)
+    fun onDeleteClicked(id: String) = accept(MainStore.Intent.Delete(id))
+    fun reload() = accept(MainStore.Intent.Load)
 }
