938d5b37b3c5333f529a61c010e1aba49b052239
UKhurshed <khurshedumarov777@gmail.com>
2026-05-24 17:18:01 +0500

    feat: подключена навигация Decompose к MainActivity

--- androidApp/src/main/kotlin/org/text/note/MainActivity.kt
+++ androidApp/src/main/kotlin/org/text/note/MainActivity.kt
@@ -4,27 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.arkivanov.decompose.defaultComponentContext
+import org.text.note.root.RootComponent
+import org.text.note.root.RootContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
+        val root = RootComponent(defaultComponentContext())
         setContent {
             MaterialTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    Text("Notes App — каркас готов")
-                }
+                RootContent(root)
             }
         }
     }
