86aad0197102f21e56a9461d250e2832ca1bbbb7
UKhurshed <khurshedumarov777@gmail.com>
2026-05-25 09:37:49 +0500

    fix: исправлена автообновления списка после возвращения на главный экран

--- androidApp/src/main/kotlin/org/text/note/MainActivity.kt
+++ androidApp/src/main/kotlin/org/text/note/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import com.arkivanov.decompose.defaultComponentContext
 import org.koin.android.ext.android.inject
+import org.text.note.feature.detail.NoteDetailStoreFactory
 import org.text.note.feature.edit.NoteEditStoreFactory
 import org.text.note.feature.main.MainStoreFactory
 import org.text.note.root.RootComponent
@@ -15,13 +16,17 @@ import org.text.note.root.RootContent
 class MainActivity : ComponentActivity() {
     private val mainStoreFactory: MainStoreFactory by inject()
     private val editStoreFactory: NoteEditStoreFactory by inject()
+
+    private val detailStoreFactory: NoteDetailStoreFactory by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         val root = RootComponent(
             defaultComponentContext(),
             mainStoreFactory = mainStoreFactory,
-            editStoreFactory = editStoreFactory
+            editStoreFactory = editStoreFactory,
+            detailStoreFactory = detailStoreFactory,
         )
         setContent {
             MaterialTheme {
